c5cb4027ce874e7cbf1c456b6c82be0769fbae15
marasulov <umr5@mail.ru>
2020-11-21 12:12:25 +0500

    create new class object copier

--- TransmittalCreator/myCommands.cs
+++ TransmittalCreator/myCommands.cs
@@ -34,12 +34,14 @@ namespace TransmittalCreator
             // Put your command code here
             Document doc = Application.DocumentManager.MdiActiveDocument;
             Editor ed;
-            if (doc != null)
-            {
+
+            var opts = new PromptSelectionOptions();
+            opts.MessageForAdding = "Select block references: [Abbb Baaa]"; 
+            
                 ed = doc.Editor;
                 ed.WriteMessage("Hello, this is your first command.helooooooo");
 
-            }
+            
         }
 
         // Modal Command with pickfirst selection
@@ -260,6 +262,46 @@ namespace TransmittalCreator
                 }
             }
         }
+
+        [CommandMethod("LockLayer")]
+
+        public static void LockLayer()
+        {
+            // Get the current document and database
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+            // Start a transaction
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the Layer table for read
+                LayerTable acLyrTbl;
+                acLyrTbl = acTrans.GetObject(acCurDb.LayerTableId,
+                    OpenMode.ForRead) as LayerTable;
+                string sLayerName = "ABC";
+                LayerTableRecord acLyrTblRec;
+                if (acLyrTbl.Has(sLayerName) == false)
+                {
+                    acLyrTblRec = new LayerTableRecord();
+                    // Assign the layer a name
+                    acLyrTblRec.Name = sLayerName;
+                    // Upgrade the Layer table for write
+                    acLyrTbl.UpgradeOpen();
+                    // Append the new layer to the Layer table and the transaction
+                    acLyrTbl.Add(acLyrTblRec);
+                    acTrans.AddNewlyCreatedDBObject(acLyrTblRec, true);
+                }
+                else
+                {
+                    acLyrTblRec = acTrans.GetObject(acLyrTbl[sLayerName],
+                        OpenMode.ForWrite) as LayerTableRecord;
+                }
+                // Lock the layer
+                acLyrTblRec.IsLocked = true;
+                // Save the changes and dispose of the transaction
+                acTrans.Commit();
+            }
+        }
+
 
         [CommandMethod("test")]
             public void Test()
