159255c0acaec4188447d69f15f0c19ccd97e8f1
marasulov <umr5@mail.ru>
2020-12-01 17:30:37 +0500

    last changes

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,17 +1,155 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using TransmittalCreator.Services;
+
 
 namespace TestConsole
 {
+    class Program
+    {
+        interface IPrinter
+        {
+            void Print(string text);
+        }
+        class ConsolePrinter : IPrinter
+        {
+            public void Print(string text)
+            {
+                Console.WriteLine(text);
+            }
+        }
+        class Report
+        {
+            public string Text { get; set; }
+            public void GoToFirstPage()
+            {
+                Console.WriteLine("Переход к первой странице");
+            }
+            public void GoToLastPage()
+            {
+                Console.WriteLine("Переход к последней странице");
+            }
+            public void GoToPage(int pageNumber)
+            {
+                Console.WriteLine("Переход к странице {0}", pageNumber);
+            }
+            public void Print(IPrinter printer)
+            {
+                printer.Print(this.Text);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+
+
+
+            File.Delete(@"C:\Users\yusufzhon.marasulov\Desktop\ВОР\new\UZLE-59-030-OPN-SCH-080103-RU-A1.docx");
+
+            MobileStore store = new MobileStore(
+                new ConsolePhoneReader(), new GeneralPhoneBinder(),
+                new GeneralPhoneValidator(), new TextPhoneSaver());
+            store.Process();
+
+            IPrinter printer = new ConsolePrinter();
+            Report report = new Report();
+            report.Text = "Hello Wolrd";
+            report.Print(printer);
+
+            Console.Read();
+
+            string filename = @"C:\Users\yusufzhon.marasulov\Desktop\1.txt";
+            List<string> text = File.ReadAllLines(filename).ToList();
+            
+            List<MatchCollection> matchCollections= new List<MatchCollection>();
+
+
+            string str3 = "ISO_expand_A1_(841.00_x_594.00_MM)";
+            string str = "UserDefinedMetric (2378.00 x 841.00мм)";
+            double width = 841.00;
+            double height = 594.00;
+            string pat = @"\d{1,}?\.\d{2}";
+
+            foreach (var line in text)
+            {
+                Regex pattern = new Regex(pat, RegexOptions.Compiled |
+                                               RegexOptions.Singleline);
+                //string str2 = Regex.Split(str, pattern);
+                if (pattern.IsMatch(line))
+                {
+                    
+                    MatchCollection str2 = pattern.Matches(line, 0);
+                    
+                    string strWidth = str2[0].ToString();
+                    string strheight = str2[1].ToString();
+                    double strWidthD = Convert.ToDouble(strWidth, System.Globalization.CultureInfo.InvariantCulture);
+                    double strheightD = Convert.ToDouble(strheight, System.Globalization.CultureInfo.InvariantCulture);
+
+                    Console.WriteLine(strWidthD);
+
+                    //double strheight = Convert.ToDouble(str2[1]);
+
+
+                    if (strWidthD == width & strheightD == height)
+                    {
+                        Console.WriteLine("{0} ширина {1}-{2}  высота {3}-{4}", line, strWidthD, width, strheightD, height);
+                        return;
+                    }
+                      
+                    matchCollections.Add(str2);
+                }
+            }
+            string list = GetWithIn(str);
+            Console.ReadLine();
+        }
+
+
+        public static double ConvertToDouble(string Value)
+        {
+            if (Value == null)
+            {
+                return 0;
+            }
+            else
+            {
+                double OutVal;
+                double.TryParse(Value, out OutVal);
+
+                if (double.IsNaN(OutVal) || double.IsInfinity(OutVal))
+                {
+                    return 0;
+                }
+                return OutVal;
+            }
+        }
+
+        public static string GetWithIn(string str)
+        {
+            string rez = "";
+
+            Regex pattern =
+                new Regex(@"\d{1,}?\.[00]{1,2}",
+                    RegexOptions.Compiled |
+                    RegexOptions.Singleline);
+
+            foreach (Match m in pattern.Matches(str))
+                if (m.Success)
+                {
+                    rez = m.Groups["val"].Value;
+
+                }
+
+            //меж скобок 
+            return rez;
+        }
+    }
+
+
+
+
     class Account
     {
         int _sum; // Переменная для хранения суммы
@@ -175,142 +313,6 @@ namespace TestConsole
     }
 
 
-    class Program
-    {
-        interface IPrinter
-        {
-            void Print(string text);
-        }
-        class ConsolePrinter : IPrinter
-        {
-            public void Print(string text)
-            {
-                Console.WriteLine(text);
-            }
-        }
-        class Report
-        {
-            public string Text { get; set; }
-            public void GoToFirstPage()
-            {
-                Console.WriteLine("Переход к первой странице");
-            }
-            public void GoToLastPage()
-            {
-                Console.WriteLine("Переход к последней странице");
-            }
-            public void GoToPage(int pageNumber)
-            {
-                Console.WriteLine("Переход к странице {0}", pageNumber);
-            }
-            public void Print(IPrinter printer)
-            {
-                printer.Print(this.Text);
-            }
-        }
-
-
-
-
-        static void Main(string[] args)
-        {
-            MobileStore store = new MobileStore(
-                new ConsolePhoneReader(), new GeneralPhoneBinder(),
-                new GeneralPhoneValidator(), new TextPhoneSaver());
-            store.Process();
-
-            IPrinter printer = new ConsolePrinter();
-            Report report = new Report();
-            report.Text = "Hello Wolrd";
-            report.Print(printer);
-
-            Console.Read();
-
-            string filename = @"C:\Users\yusufzhon.marasulov\Desktop\1.txt";
-            List<string> text = File.ReadAllLines(filename).ToList();
-            
-            List<MatchCollection> matchCollections= new List<MatchCollection>();
-
-
-            string str3 = "ISO_expand_A1_(841.00_x_594.00_MM)";
-            string str = "UserDefinedMetric (2378.00 x 841.00мм)";
-            double width = 841.00;
-            double height = 594.00;
-            string pat = @"\d{1,}?\.\d{2}";
-
-            foreach (var line in text)
-            {
-                Regex pattern = new Regex(pat, RegexOptions.Compiled |
-                                               RegexOptions.Singleline);
-                //string str2 = Regex.Split(str, pattern);
-                if (pattern.IsMatch(line))
-                {
-                    
-                    MatchCollection str2 = pattern.Matches(line, 0);
-                    
-                    string strWidth = str2[0].ToString();
-                    string strheight = str2[1].ToString();
-                    double strWidthD = Convert.ToDouble(strWidth, System.Globalization.CultureInfo.InvariantCulture);
-                    double strheightD = Convert.ToDouble(strheight, System.Globalization.CultureInfo.InvariantCulture);
-
-                    Console.WriteLine(strWidthD);
-
-                    //double strheight = Convert.ToDouble(str2[1]);
-
-
-                    if (strWidthD == width & strheightD == height)
-                    {
-                        Console.WriteLine("{0} ширина {1}-{2}  высота {3}-{4}", line, strWidthD, width, strheightD, height);
-                        return;
-                    }
-                      
-                    matchCollections.Add(str2);
-                }
-            }
-            string list = GetWithIn(str);
-            Console.ReadLine();
-        }
-
-
-        public static double ConvertToDouble(string Value)
-        {
-            if (Value == null)
-            {
-                return 0;
-            }
-            else
-            {
-                double OutVal;
-                double.TryParse(Value, out OutVal);
-
-                if (double.IsNaN(OutVal) || double.IsInfinity(OutVal))
-                {
-                    return 0;
-                }
-                return OutVal;
-            }
-        }
-
-        public static string GetWithIn(string str)
-        {
-            string rez = "";
-
-            Regex pattern =
-                new Regex(@"\d{1,}?\.[00]{1,2}",
-                    RegexOptions.Compiled |
-                    RegexOptions.Singleline);
-
-            foreach (Match m in pattern.Matches(str))
-                if (m.Success)
-                {
-                    rez = m.Groups["val"].Value;
-
-                }
-
-            //меж скобок 
-            return rez;
-        }
-    }
 
     public class Params
     {
